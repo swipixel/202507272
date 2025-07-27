ae722c6bbfdc6ca521a899b523bdae8b06c10536
swipixel <swipixel@gmail.com>
2025-07-27 19:52:18 +0200

    Updated i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -18,7 +18,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'hu', // default language
+    lng: 'en', // default language
     fallbackLng: 'en', // fallback language if translation is not found
     interpolation: {
       escapeValue: false // react already safes from xss
