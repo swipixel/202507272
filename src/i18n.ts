148d7d28a583e1b5e40432510c34580d23321218
swipixel <swipixel@gmail.com>
2025-07-27 19:53:00 +0200

    Updated i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector'; // Import LanguageDetector
 
 // Import translation files
 import enTranslation from './locales/en/translation.json';
@@ -15,11 +16,16 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector) // Use the language detector
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'en', // default language
+    // lng: 'en', // Remove or comment out this line to let the detector decide
     fallbackLng: 'en', // fallback language if translation is not found
+    detection: {
+      order: ['navigator', 'localStorage', 'cookie'], // Order of detection sources
+      caches: ['localStorage'], // Cache detected language
+    },
     interpolation: {
       escapeValue: false // react already safes from xss
     },
